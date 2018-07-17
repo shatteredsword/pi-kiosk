eb3ad898bdb0ba6e0c65fb4a44181f4ad2f2788c
Sean Mayo <sean.mayo@matsonfruit.com>
2018-07-16 17:43:17 -0700

    trying to slim down the repo

--- install.sh
+++ install.sh
@@ -1,12 +1,59 @@
 #!/usr/bin/env bash
 sudo apt-get -y update
-sudo apt-get -y install firefox-esr samba xscreensaver unclutter
+sudo apt-get -y install firefox-esr samba xscreensaver unclutter omxplayer
 sudo adduser --system shareuser
 sudo mkdir -m 1777 /home/pi/pi-kiosk/kiosk
 sudo chown shareuser /home/pi/pi-kiosk/kiosk
 sudo chmod 777 -R /home/pi/pi-kiosk/kiosk
 sudo wget -O /home/pi/pi-kiosk/kiosk/video.mp4 https://raw.githubusercontent.com/mediaelement/mediaelement-files/master/big_buck_bunny.mp4
-sudo cp /home/pi/pi-kiosk/smb.conf /etc/samba/smb.conf
+sudo tee /etc/samba/smb.conf > /dev/null <<EOF
+[global]
+   workgroup = WORKGROUP
+   dns proxy = no
+   log file = /var/log/samba/log.%m
+   max log size = 1000
+   syslog = 0
+   panic action = /usr/share/samba/panic-action %d
+   server role = standalone server
+   passdb backend = tdbsam
+   obey pam restrictions = yes
+   unix password sync = yes
+   passwd program = /usr/bin/passwd %u
+   passwd chat = *Enter\snew\s*\spassword:* %n\n *Retype\snew\s*\spassword:* %n\n *password\supdated\ssuccessfully* .
+   pam password change = yes
+   map to guest = bad user
+   usershare allow guests = yes
+[homes]
+   comment = Home Directories
+   browseable = no
+   read only = yes
+   create mask = 0700
+   directory mask = 0700
+   valid users = %S
+[printers]
+   comment = All Printers
+   browseable = no
+   path = /var/spool/samba
+   printable = yes
+   guest ok = no
+   read only = yes
+   create mask = 0700
+[print$]
+   comment = Printer Drivers
+   path = /var/lib/samba/printers
+   browseable = yes
+   read only = yes
+   guest ok = no
+[kiosk]
+   path = /home/pi/pi-kiosk/kiosk
+   browseable = yes
+   writeable = yes   
+   read only = no
+   guest ok = yes
+   create mask = 777
+   directory mask = 777
+   force user = shareuser
+EOF
 sudo /etc/init.d/samba restart
 tee /home/pi/autostart.sh > /dev/null <<EOF
 #!/usr/bin/env bash
@@ -23,4 +70,4 @@ tee /home/pi/.config/lxsession/LXDE-pi/autostart > /dev/null <<EOF
 @point-rpi
 @bash /home/pi/autostart.sh
 EOF
-sudo cp /home/pi/pi-kiosk/\{d320c473-63c2-47ab-87f8-693b1badb5e3\}.xpi /usr/share/mozilla/extensions/\{ec8030f7-c20a-464f-9b0e-13a3a9e97384\}/
+sudo wget -O "/usr/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}/{d320c473-63c2-47ab-87f8-693b1badb5e3}.xpi" "https://addons.mozilla.org/firefox/downloads/file/860622/autofullscreen-1.0.0.1-an+fx.xpi"
