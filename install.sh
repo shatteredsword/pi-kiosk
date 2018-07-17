7d158ab26884f5d3239d38ac595eb32b2967f10e
Sean Mayo <sean.mayo@matsonfruit.com>
2018-07-16 17:12:43 -0700

    permissions update

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@
 sudo apt-get -y update
 sudo apt-get -y install firefox-esr samba xscreensaver unclutter
 sudo adduser --system shareuser
-sudo mkdir -m 1755 /home/pi/pi-kiosk/kiosk
+sudo mkdir -m 1777 /home/pi/pi-kiosk/kiosk
 sudo chown shareuser /home/pi/pi-kiosk/kiosk
+sudo chmod 777 -R /home/pi/pi-kiosk/kiosk
 sudo wget -O /home/pi/pi-kiosk/kiosk/video.mp4 https://raw.githubusercontent.com/mediaelement/mediaelement-files/master/big_buck_bunny.mp4
 sudo cp /home/pi/pi-kiosk/smb.conf /etc/samba/smb.conf
 sudo /etc/init.d/samba restart
@@ -23,4 +24,3 @@ tee /home/pi/.config/lxsession/LXDE-pi/autostart > /dev/null <<EOF
 @bash /home/pi/autostart.sh
 EOF
 sudo cp /home/pi/pi-kiosk/\{d320c473-63c2-47ab-87f8-693b1badb5e3\}.xpi /usr/share/mozilla/extensions/\{ec8030f7-c20a-464f-9b0e-13a3a9e97384\}/
-
