a513339b28efa87e135a58161884dced35b0288e
Sean Mayo <sean.mayo@matsonfruit.com>
2019-03-22 11:16:06 -0700

    changed relative paths to absolute

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 while true
 do
-	omxplayer -o hdmi kiosk/* 2>> error.log 1> /dev/null
+	omxplayer -o hdmi /home/pi/pi-kiosk/kiosk/* 2>> /home/pi/pi-kiosk/error.log 1> /dev/null
 done
